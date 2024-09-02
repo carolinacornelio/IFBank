9aa8f1849e22fdcbf40e8e142f74592db60bdcab
Ana Carolina Cornélio <163690998+carolinacornelio@users.noreply.github.com>
2024-09-01 21:31:48 -0300

    Switch.java

--- src/banco/Banco.java
+++ src/banco/Banco.java
@@ -1,5 +1,4 @@
 package banco;
-
 import java.util.*;
 
 public class Banco{
@@ -15,42 +14,24 @@ public class Banco{
   }
 
   public String retirarFicha(int tipo){
-    if (tipo == 1) { // Preferencial
-      ultimoPreferencial++;
-      String ficha = "P-" + ultimoPreferencial;
-      filaPreferencial.add(ficha);
-      return "\nFicha preferencial retirada: " + ficha;
-    } else if (tipo == 2) { // Comum
-      ultimoComum++;
-      String ficha = "C-" + ultimoComum;
-      filaComum.add(ficha);
-      return "\nFicha comum retirada: " + ficha;
-    } else {
-      return "Tipo de ficha inválido! Tente novamente.";
+   switch (tipo){
+	case 1: // preferencial
+	    ultimoPreferencial++;
+	    String ficha = "P-" + ultimoPreferencial;
+	    filaPreferencial.add(ficha);
+	    return "\nFicha preferencial retirada: " + ficha;
+	    break;
+	case 2: // comum
+	    ultimoComum++;
+	    String ficha = "C-" + ultimoComum;
+	    filaComum.add(ficha);
+            return "\nFicha comum retirada: " + ficha;
+	    break;
+	default:
+	    return "Tipo de ficha inválido! Tente novamente.";
+	    break;
     }
-  }
-
-  /* Testar com o switch
-  public String retirarFicha(int tipo){
-	    switch (tipo){
-	        case 1: // preferencial
-	            ultimoPreferencial++;
-                String ficha = "P-" + ultimoPreferencial;
-                filaPreferencial.add(ficha);
-                return "\nFicha preferencial retirada: " + ficha;
-                break;
-            case 2: // comum
-                ultimoPreferencial++;
-                String ficha = "P-" + ultimoPreferencial;
-                filaPreferencial.add(ficha);
-                return "\nFicha preferencial retirada: " + ficha;
-                break;
-            default:
-                return "Tipo de ficha inválido! Tente novamente.";
-                break;
-	    }
-  }
-  */
+}
 
   public String chamarProximo(){
     if (filaPreferencial.isEmpty() && filaComum.isEmpty()) {
@@ -67,15 +48,4 @@ public class Banco{
 
       return "Próxima ficha chamada: " + proximaFicha;
   }
-
-  public String proximaFicha() {
-      if (!filaPreferencial.isEmpty() && (chamadasPreferenciais < 2 || filaComum.isEmpty())) {
-          return "Próxima ficha será preferencial: " + filaPreferencial.get(0);
-      } else if (!filaComum.isEmpty()) {
-          return "Próxima ficha será comum: " + filaComum.get(0);
-      } else {
-          return "Nenhuma ficha disponível para ser chamada.";
-      }
-  }
-
 }
